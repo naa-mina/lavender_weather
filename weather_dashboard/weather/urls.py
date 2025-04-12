ea7d1dbbf019d53613d060850be2506dad8e89c0
mina_b <105630816+naa-mina@users.noreply.github.com>
2025-04-12 14:15:58 +0000

    Update logout path

--- weather_dashboard/weather/urls.py
+++ weather_dashboard/weather/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('weather/<str:city>/',views.get_weather),   # GET weather for a city
     path('register/',views.register),                # POST to register user
     path('login/', views.login),                     #POST to login                                           
-    path('logout/', views.logout),                  #POST to logout
+    path('logout/', views.logout_view),                  #POST to logout
     path('favorites/', views.favorite_cities),      #GET/POST favorite cities
 ]
